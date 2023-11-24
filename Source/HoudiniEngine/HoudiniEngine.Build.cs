ef6f9bac7f21dc1b1eabb6966a8d064656a41867
Morne Chamberlain <mornechamberlain@sidefx.com>
2023-11-24 08:46:59 -0500

    Bumping to Houdini Version: 20.5.64 Houdini Engine Version: 6.0 (API: 0)

--- Source/HoudiniEngine/HoudiniEngine.Build.cs
+++ Source/HoudiniEngine/HoudiniEngine.Build.cs
@@ -32,7 +32,7 @@
 
 /*
 
-    Houdini Version: 20.5.62
+    Houdini Version: 20.5.64
     Houdini Engine Version: 6.0.0
     Unreal Version: 5.0.0
 
@@ -78,7 +78,7 @@ public class HoudiniEngine : ModuleRules
 
     private string GetHFSPath()
     {
-        string HoudiniVersion = "20.5.62";
+        string HoudiniVersion = "20.5.64";
         bool bIsRelease = true;
         string HFSPath = "";
         string RegistryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Side Effects Software";
@@ -373,7 +373,8 @@ public class HoudiniEngine : ModuleRules
                     "SubobjectEditor",
                     "MeshMergeUtilities",
                     "MeshUtilitiesCommon",
-                    "Json"
+                    "Json",
+                    "SkeletalMeshDescription"
                 }
             );
         }
